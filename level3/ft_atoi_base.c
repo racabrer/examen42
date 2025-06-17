99592c42c4e9f4fa31b721a0d12b426b436701ca
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-06-17 15:55:46 +0200

    arreglando atoibase

--- level3/ft_atoi_base.c
+++ level3/ft_atoi_base.c
@@ -20,58 +20,51 @@ Your function must be declared as follows:
 
 int	ft_atoi_base(const char *str, int str_base);
 */
+#include <unistd.h>
+
+int isspace(int c)
+{
+    return (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32);
+}
 
 int isvalid(int ch, int baselen)
 {
-	//Definir la base 
-	char *lcbase = "0123456789abcdef";
-	char *ucbase = "0123456789ABCDEF";;
-	int i = 0;
-
-	while(i < baselen)
-	{
-		if (ch == lcbase[i] || ch == ucbase[i])
-			return(1);
-		i++;
-	}
-	return(0);
-}
-int isspace(int c)
-{
-	if (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32)
-		return (1);
-	return(0);
+    return ((ch >= '0' && ch <= '9') || 
+            (baselen > 10 && ((ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))));
 }
 
-int	ft_atoi_base(const char *str, int str_base)
+int ft_atoi_base(const char *str, int str_base)
 {
-	int res = 0;
-	int sign = 1;
-	int i = 0;
-	
-	while(isspace(str[i]))
-		i++;
-	if (str[i] == '+' && str[i + 1] != '-')
-		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	while (str[i] && isvalid(str[i], str_base))
-	{
-		res *= str_base;
-		if(str[i] > '0' && str[i] <= '9')
-			res += str[i] - '0';
-		else if (str[i] >= 'a' && str[i] <= 'f')
-			res += str[i] - 'a' + 10;
-		else if (str[i] >= 'A' && str[i] <= 'F')
-			res += str[i] - 'A' + 10;
-		i++;
-	}
-	return (res *sign);
+    int res = 0;
+    int sign = 1;
+    int i = 0;
+    
+    if (str_base < 2 || str_base > 16)
+        return (0);
+    while (isspace(str[i]))
+        i++;
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (str[i] == '+')
+        i++;
+    while (str[i] && isvalid(str[i], str_base))
+    {
+        res *= str_base;
+        if (str[i] >= '0' && str[i] <= '9')
+            res += str[i] - '0';
+        else if (str[i] >= 'a' && str[i] <= 'f')
+            res += str[i] - 'a' + 10;
+        else if (str[i] >= 'A' && str[i] <= 'F')
+            res += str[i] - 'A' + 10;
+        i++;
+    }
+    return (res * sign);
 }
-//Uncomment the main to test the function
+
+
 #include <stdio.h>
 int main(int argc, char **argv)
 {
