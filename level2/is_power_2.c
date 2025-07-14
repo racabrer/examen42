66f54c980735c6963a915d48e6c4eeff9e305e76
racabrer <racabrer@student.42madrid.com>
2025-07-14 15:47:28 +0200

    Update is_power_2.c

--- level2/is_power_2.c
+++ level2/is_power_2.c
@@ -13,69 +13,22 @@ Your function must be declared as follows:
 int	    is_power_of_2(unsigned int n);
 */
 
-int	    is_power_of_2(unsigned int n)
+int	is_power_of_2(unsigned int n)
 {
-	/*
-		we will initialize a test variable to 1, and we will multiply it by 2 
-		until is equal to or greater than the variable we have been given as a 
-		parameter (n). If the 2 variables are equal it means that is a power of 2 
-		(since we have always multiplied this number by 2).
-	*/
-
-/*
-int	    is_power_of_2(unsigned int n)
-{
-    if (n == 0)
-        return (0);
-    while (n % 2 == 0)
-        n = n / 2;
-    return (n == 1);
-}
-
-#include <stdio.h>
-
-int is_power_of_2(unsigned int n);
-
-int main(void)
-{
-    printf("0: %d\n", is_power_of_2(0));
-    printf("1: %d\n", is_power_of_2(1));
-    printf("2: %d\n", is_power_of_2(2));
-    printf("3: %d\n", is_power_of_2(3));
-    printf("4: %d\n", is_power_of_2(4));
-    printf("5: %d\n", is_power_of_2(5));
-    printf("8: %d\n", is_power_of_2(8));
-    printf("16: %d\n", is_power_of_2(16));
-    printf("31: %d\n", is_power_of_2(31));
-    printf("32: %d\n", is_power_of_2(32));
-    printf("33: %d\n", is_power_of_2(33));
-    printf("64: %d\n", is_power_of_2(64));
-    printf("127: %d\n", is_power_of_2(127));
-    printf("128: %d\n", is_power_of_2(128));
-
-    return 0;
-}
-
-*/
-	int test;
-
-	test = 1;
-	while (test <= n)
-	{
-		if (test == n)
-			return (1); //test is a power of 2
-		test = test * 2;
-	} 
-	/*
-		we will leave the loop if the test variable is greater than n, this means 
-		that it is not a power of 2 and we need to retun 0.
-	*/
+	if (n == 0)
+		return (0);
+	while (n % 2 == 0)
+		n /= 2;
+	if (n == 1)
+		return (1);
 	return (0);
 }
-
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main()
 {
-	printf("%d", is_power_of_2(16));
-}
+	int n = 16;
+	printf ("%d", is_power_of_2(n));
+	return (0);
+}*/
+
